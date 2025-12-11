413c51ea21359f7365183b7f6361bf5cfad3ae97
Mj-Ndalasi <mj.ndalasi@gmail.com>
2025-12-11 14:44:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ with tab1:
                 
                 try:
                     response = client.models.generate_content(
-                        model='gemini-1.5-flash', 
+                        model='gemini-2.5-flash', 
                         contents=prompt,
                         config=types.GenerateContentConfig(
                             tools=[google_search_tool],
